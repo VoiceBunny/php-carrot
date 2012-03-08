5bfa218104b3c9b9b8e4f87def89ad15a15036ab
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2012-03-08 15:48:16 -0500

    base code

--- example.php
+++ example.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once('lib/PHPCarrot.php');
+require_once('lib/VoiceBunnyCarrot.php');
 
-$vb_carrot = new VBCarrot('85','de52c722a40ea875ce89ae134a9ad66071144f5e','https://api.local.voicebunny.com/');
+$vb_carrot = new VoiceBunnyCarrot('85','de52c722a40ea875ce89ae134a9ad66071144f5e');
 echo "<br/>";
 print_r($vb_carrot->languages());
 ?>
